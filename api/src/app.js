f6940cdc3a8a3a7b0575f56719d733efd8318cc8
Jayson Clark <jayson.clark0421@gmail.com>
2024-06-26 16:06:31 -0400

    Setup express server

--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,10 @@
-import db from './lib/db.js';
+import express, { json } from 'express';
+import cors from 'cors';
+import db from './lib/db.js'; 
+
+const app = express();
+app.use(cors()); 
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
